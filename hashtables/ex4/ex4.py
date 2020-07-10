60bd55916e7989f1711ed2839d3f84bef398f7b4
evan <aspaasevan@gmail.com>
2020-07-10 10:31:26 -0700

    stretch ex4 tests passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,8 +2,22 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
+    ht = {}
 
+    # approach, add each negative number to dictionary as a key with value of true
+    for negative in a:
+        if negative < 0:
+            ht[negative] = negative
+
+    # compare each positive number to value in dict and append to result
+    for num in a:
+        # use of try/except to allow passing tests
+        try:
+            if ht[-num]:
+                result.append(num)
+        except:
+            pass
     return result
 
 
