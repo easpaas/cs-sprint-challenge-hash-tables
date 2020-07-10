e7fc1eaa55fd47dfd709705f056cc2acd0823878
evan <aspaasevan@gmail.com>
2020-07-10 09:25:27 -0700

    ex1 passing all 4 tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,20 +1,28 @@
 from collections import defaultdict 
 
 # initialize hash table as built-in 
-ht = defaultdict()
+# ht = defaultdict()
 
 
 def get_indices_of_item_weights(weights, length, limit):
-  # my code here 
-  global ht
-  
+  ht = {}
+
   #  store each weight in weights input list as a key in ht
   # with each key, find another key whos sum equals limit and store as value
   for i in range(length):
-    ht.fromkeys(weights[i])
+    # determines difference needed to reach limit
+    remaining = limit - weights[i]
     
-  print(f"hash table keys: {ht}")
-  # return None
+    # check if remaining exists in ht
+    if remaining in ht:
+      # output: ht[remaining]
+      output = ht[remaining]
+      result = [i,output]
+      result.sort(reverse=True)
+      # print(f"result: {result}")
+      return result
+    else:
+      ht[weights[i]] = i
+  # print(f"hash table keys: {ht}")
+  return None
 
-
-get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21)
