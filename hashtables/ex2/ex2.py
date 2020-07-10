e6ad330cbf9fa1a40ba0325c108121e96ca95204
evan <aspaasevan@gmail.com>
2020-07-10 10:00:05 -0700

    ex2 passing tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,31 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
+        # starting airport
         self.source = source
+        # next airport
         self.destination = destination
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    ht = {}
+    trip = []
 
-    return route
+    # for each Ticket in tickets 
+    for ticket in tickets:
+        # link tickets via ticket.destination == ticket.source
+        ht[ticket.source] = ticket.destination
+        
+    # keep index in range
+    trip.append(ht['NONE'])
+
+    for i in range(length): 
+        # for each Key in ht
+        if trip[i] in ht:
+            # if destination is equal to source
+            if ht[trip[i]] == trip[0]:
+                continue
+            trip.append(ht[trip[i]])
+
+    return trip
+
+    # return route
