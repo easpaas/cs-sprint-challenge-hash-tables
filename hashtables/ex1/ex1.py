d790c4aa2f51e4a680924dc3450a9538def9f828
evan <aspaasevan@gmail.com>
2020-07-10 09:13:14 -0700

    working progress on ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,20 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+from collections import defaultdict 
 
-    return None
+# initialize hash table as built-in 
+ht = defaultdict()
+
+
+def get_indices_of_item_weights(weights, length, limit):
+  # my code here 
+  global ht
+  
+  #  store each weight in weights input list as a key in ht
+  # with each key, find another key whos sum equals limit and store as value
+  for i in range(length):
+    ht.fromkeys(weights[i])
+    
+  print(f"hash table keys: {ht}")
+  # return None
+
+
+get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21)
